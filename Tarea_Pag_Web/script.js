b411903cd8d51e1725019982945957279fe08f96
AmiyaMihari <diaztamaki@gmail.com>
2025-06-08 21:43:25 -0600

    [ADD] página web

---
+++ Tarea_Pag_Web/script.js
@@ -0,0 +1,24 @@
+// Espera a que todo el contenido de la página se cargue
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Seleccionamos el video y el botón por su ID
+    const video = document.getElementById('bg-video');
+    const pauseButton = document.getElementById('pause-button');
+
+    // Añadimos un "escuchador de eventos" que se activa cuando hacemos clic en el botón
+    pauseButton.addEventListener('click', () => {
+        // Si el video está en pausa...
+        if (video.paused) {
+            // ...lo reproducimos
+            video.play();
+            // y cambiamos el ícono del botón a "pausa"
+            pauseButton.innerHTML = '❚❚';
+        } else { // Si el video se está reproduciendo...
+            // ...lo pausamos
+            video.pause();
+            // y cambiamos el ícono del botón a "reproducir"
+            pauseButton.innerHTML = '▶';
+        }
+    });
+
+});
